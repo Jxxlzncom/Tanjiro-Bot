7ed6111b6ca02eed6ae560b0c0ea47fe37b6299e
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-06-29 23:04:28 -0500

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 // ╭─────────────────────⊷
-// │ 𖥔 MaycolAIUltraMD - Configuración
-// │ Tematizado al estilo Hanako-Kun (◍•ᴗ•◍)❤
+// │ 𖥔 Tanjiro - Configuración
+// │ Bot basado en MayBailyes
 // ╰─────────────────────⊷
 
 import { watchFile, unwatchFile } from 'fs'
@@ -16,80 +16,77 @@ import moment from 'moment-timezone'
 global.botNumber = '' // Ejemplo: 51921826291
 
 // ╭── ✦ Propietario y Staff ✦ ──⊷
-global.owner = ['51921826291', '50433069018']
+global.owner = ['51939260619', '51921826291']
 global.mods = []
 global.suittag = []
 global.prems = []
-global.creador = '51921826291'
-global.namechannel = '𝐒𝐨𝐲𝐌𝐚𝐲𝐜𝐨𝐥 <𝟑 • Actualizaciones'
-global.namechannel2 = '𝐒𝐨𝐲𝐌𝐚𝐲𝐜𝐨𝐥 <𝟑 • Actualizaciones'
-global.namegrupo = '𝐒𝐨𝐲𝐌𝐚𝐲𝐜𝐨𝐥 <𝟑 • Actualizaciones'
-global.namecomu = '𝐒𝐨𝐲𝐌𝐚𝐲𝐜𝐨𝐥 <𝟑 • Actualizaciones'
-global.apodo = '𝚂𝚘𝚢𝙼𝚊𝚢𝚌𝚘𝚕 <𝟹'
-global.repo = 'SoySapo6/MaycolAIUltraMD'
+global.creador = '51939260619'
+global.namechannel = '𝖙𝖆𝖓𝖏𝖎𝖗𝖔 𝖐𝖆𝖒𝖆𝖉𝖔'
+global.namechannel2 = '𝖙𝖆𝖓𝖏𝖎𝖗𝖔 𝖐𝖆𝖒𝖆𝖉𝖔'
+global.namegrupo = '𝖙𝖆𝖓𝖏𝖎𝖗𝖔 𝖐𝖆𝖒𝖆𝖉𝖔'
+global.namecomu = '𝖙𝖆𝖓𝖏𝖎𝖗𝖔 𝖐𝖆𝖒𝖆𝖉𝖔'
+global.apodo = 'ᴅᴀʀᴋ ʙʀxᴢᴢᴢ'
+global.repo = 'SoySapo6/Tanjiro-Bot'
 global.pais = '⊹˚• Perú •˚⊹'
-global.github = 'SoySapo6'
+global.github = 'Jxxlzncom'
 
 // ╭── ✦ Información del Bot ✦ ──⊷
 global.libreria = 'MayBailyes'
 global.baileys = 'V 6.7.16'
 global.vs = '2.2.0'
-global.nameqr = 'SoyMaycol'
-global.namebotttt = 'MaycolAIUltraMD'
-global.namebot = '𝐌𝐚𝐲𝐜𝐨𝐥𝐀𝐈𝐔𝐥𝐭𝐫𝐚-𝐌𝐃'
-global.personaje = 'Hanako Kun'
+global.nameqr = 'Tanjiro'
+global.namebotttt = 'TanjiroBot'
+global.namebot = '𝖙𝖆𝖓𝖏𝖎𝖗𝖔 𝖐𝖆𝖒𝖆𝖉𝖔'
 global.sessions = './MayBots/Principal'
 global.jadi = 'MayBots'
 global.yukiJadibts = true
 
 // ╭── ✦ Personalización Visual ✦ ──⊷
-global.packname = '𝕄𝕒𝕪𝕔𝕠𝕝𝔸𝕀𝕌𝕝𝕥𝕣𝕒-𝕄𝔻'
-global.botname = '𝐌𝐚𝐲𝐜𝐨𝐥𝐀𝐈𝐔𝐥𝐭𝐫𝐚-𝐌𝐃'
-global.wm = '𝙼𝚊𝚢𝚌𝚘𝚕𝙰𝙸𝚄𝚕𝚝𝚛𝚊-𝙼𝙳'
-global.author = '𝙃𝙚𝙘𝙝𝙤 𝙥𝙤𝙧 𝙎𝙤𝙮𝙈𝙖𝙮𝙘𝙤𝙡 <3'
+global.packname = '𝐓𝐀𝐍𝐉𝐈𝐑𝐎-𝐁𝐎𝐓 𝐌𝐃'
+global.botname = '𝐓𝐀𝐍𝐉𝐈𝐑𝐎-𝐁𝐎𝐓 𝐌𝐃'
+global.wm = '𝐓𝐀𝐍𝐉𝐈𝐑𝐎-𝐁𝐎𝐓 𝐌𝐃'
+global.author = 'ᴅᴀʀᴋ ʙʀxᴢᴢᴢ'
 global.dev = global.author
-global.textbot = '𝐌𝐚𝐲𝐜𝐨𝐥𝐀𝐈𝐔𝐥𝐭𝐫𝐚-𝐌𝐃 • 𝙃𝙚𝙘𝙝𝙤 𝙥𝙤𝙧 𝙎𝙤𝙮𝙈𝙖𝙮𝙘𝙤𝙡 <3'
-global.etiqueta = '𝙼𝚊𝚢𝚌𝚘𝚕𝙰𝙸'
+global.textbot = '𝐓𝐀𝐍𝐉𝐈𝐑𝐎-𝐁𝐎𝐓 𝐌𝐃 • ᴅᴀʀᴋ ʙʀxᴢᴢᴢ'
+global.etiqueta = 'ᴅᴀʀᴋ ʙʀxᴢᴢᴢ'
 global.listo = 'Aca lo tienes ୧⁠(⁠＾⁠ ⁠〰⁠ ⁠＾⁠)⁠୨'
 global.rwait = '🕒'
-global.done = '🇯🇵'
-global.error = '🈲'
+global.done = '🌊'
+global.error = '⚔️'
 global.msm = '⚠︎'
 global.emoji = '🇯🇵'
-global.emoji2 = '💕'
+global.emoji2 = '🏹'
 global.emoji3 = '😍'
-global.emoji4 = '🥴'
+global.emoji4 = '🫡'
 global.emoji5 = '👻'
-global.wait = '¡Esperame (⁠´⁠∩⁠｡⁠•⁠ ⁠ᵕ⁠ ⁠•⁠｡⁠∩⁠`⁠)!';
-global.waitt = '¡Esperame (⁠´⁠∩⁠｡⁠•⁠ ⁠ᵕ⁠ ⁠•⁠｡⁠∩⁠`⁠)!';
-global.waittt = '¡Esperame (⁠´⁠∩⁠｡⁠•⁠ ⁠ᵕ⁠ ⁠•⁠｡⁠∩⁠`⁠)!';
-global.waitttt = '¡Esperame (⁠´⁠∩⁠｡⁠•⁠ ⁠ᵕ⁠ ⁠•⁠｡⁠∩⁠`⁠)!';
+global.wait = '¡Esperame Cazador!';
+global.waitt = '¡Esperame!';
+global.waittt = '¡Espérame! Se pacienteee';
+global.waitttt = '¡Esperame a Tanjiro!';
 
 // ╭── ✦ Configuración General ✦ ──⊷
-global.moneda = 'MayCoins'
+global.moneda = 'Coins'
 global.welcom1 = '❍ Edita Con El Comando setwelcome'
 global.welcom2 = '❍ Edita Con El Comando setbye'
-global.banner = 'https://files.catbox.moe/l8ohvs.jpeg'
-global.banner2 = 'https://files.catbox.moe/l8ohvs.jpeg'
-global.avatar = 'https://files.catbox.moe/uvc28a.jpeg'
-global.video = 'https://files.catbox.moe/ks0qz0.mp4'
+global.banner = 'https://files.catbox.moe/xc7841.jpeg'
+global.banner2 = 'https://files.catbox.moe/xc7841.jpeg'
+global.avatar = 'https://files.catbox.moe/3qyzmz.jpg'
+global.video = 'https://files.catbox.moe/095s6m.mp4'
 global.video2 = [
-  'https://files.catbox.moe/i74z9e.mp4'
+  'https://files.catbox.moe/095s6m.mp4'
 ]
-global.icono = 'https://files.catbox.moe/wnx3j7.jpeg'
-
 // ╭── ✦ Enlaces Oficiales ✦ ──⊷
-global.gp1 = 'https://chat.whatsapp.comEprzCjr7XFyAIFr9OfBh2o'
-global.comunidad1 = 'https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R'
+global.gp1 = 'https://whatsapp.com/channel/0029Vb5OYfu3bbV5DT6Biy12'
+global.comunidad1 = 'https://whatsapp.com/channel/0029Vb5OYfu3bbV5DT6Biy12'
 global.channel = global.comunidad1
 global.channel2 = global.comunidad1
-global.md = 'https://github.com/SoySapo6/MaycolAI'
-global.correo = 'karatekidamericatv@gmail.com'
+global.md = 'https://github.com/SoySapo6/Tanjiro-Bot'
+global.correo = 'jxxlznexe@gmail.com'
 global.cn = global.comunidad1
 global.owner_ngl = 'soymaycoldev'
-global.canalIdM = ["120363372883715167@newsletter"]
-global.canalNombreM = ["𝐒𝐨𝐲𝐌𝐚𝐲𝐜𝐨𝐥 <𝟑 • Actualizaciones"]
-global.canalLink = ["https://whatsapp.com/channel/0029VayXJte65yD6LQGiRB0R"]
+global.canalIdM = ["120363403106079008@newsletter"]
+global.canalNombreM = ["𝖙𝖆𝖓𝖏𝖎𝖗𝖔 𝖐𝖆𝖒𝖆𝖉𝖔"]
+global.canalLink = ["https://whatsapp.com/channel/0029Vb5OYfu3bbV5DT6Biy12"]
 
 // ╭── ✦ Catálogo y Estilo ✦ ──⊷
 global.catalogo = fs.readFileSync('./src/catalogo.jpg')
@@ -110,7 +107,7 @@ global.estilo = {
 
 // ╭── ✦ Otros Ajustes ✦ ──⊷
 global.ch = {
-  ch1: '120363372883715167@newsletter'
+  ch1: '120363403106079008@newsletter'
 }
 global.multiplier = 70
 global.activeSocket = null
